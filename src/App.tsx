26278e073cbfab2b3d3cccd9c459e4661441b8b7
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-14 17:47:11 +0500

    Feature: Install chadcn/ui with button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import {Button} from "@/components/ui/button.tsx";
+
 function App() {
   return (
-    <>
-      <div className="bg-green-500 text-green-50">My habit tracker</div>
-    </>
+    <main className="min-h-screen px-32 pt-48">
+      <div className="bg-blue-100 px-4 py-6 rounded-2xl outline-2 outline-blue-300">
+        <p className='mb-1'>Do you want to hear about the latest news?</p>
+        <Button>Take Me 🤩</Button>
+      </div>
+    </main>
   );
 }
 
