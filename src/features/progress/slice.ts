a96e050c384a39ac5680250d48ed8304b1bdefb7
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-15 17:54:12 +0500

    Feature: Create toggleProgressStatus action

--- src/features/progress/slice.ts
+++ src/features/progress/slice.ts
@@ -1,10 +1,40 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, type PayloadAction} from "@reduxjs/toolkit";
 import {FAKE_PRORGRESS_DATA} from "@/features/progress/fakeData.ts";
 
 const progressSlice = createSlice({
   name: "progress",
   initialState: FAKE_PRORGRESS_DATA,
-  reducers: {},
+  reducers: {
+    toggleProgressStatus: (
+      state,
+      action: PayloadAction<{ date: string; habitId: string }>,
+    ) => {
+      // Check if progress is sored
+      const existingProgressIndex = state.findIndex(
+        (progress) =>
+          progress.date === action.payload.date &&
+          progress.habitId === action.payload.habitId,
+      );
+      const progress = state[existingProgressIndex];
+
+      // Toggle if status exists
+      if (existingProgressIndex > -1) {
+        progress.status = progress.status === "completed" ? null : "completed";
+      }
+
+      // Create new progress if not
+      else {
+        const newProgress = {
+          habitId: "dsds",
+          date: "p", // YYYY-MM-DD
+          status: "completed" as const,
+          updatedAt: "dsd", // ISO 8601
+        };
+        state.push(newProgress);
+      }
+    },
+  },
 });
 
+export const { toggleProgressStatus } = progressSlice.actions;
 export default progressSlice;
