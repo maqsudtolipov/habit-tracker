4510e2731195042ef99053faf53e2ee08852580f
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-15 12:29:31 +0500

    Feature: Create habits slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,11 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore} from "@reduxjs/toolkit";
+import habitsSlice from "@/features/habits/slice.ts";
 
 export const store = configureStore({
-    reducer: {},
-})
+  reducer: {
+    habits: habitsSlice.reducer,
+  },
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ src/features/habits/slice.ts
@@ -0,0 +1,11 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const habitsSlice = createSlice({
+  name: "habits",
+  initialState: {
+    test: "ok",
+  },
+  reducers: {},
+});
+
+export default habitsSlice;
