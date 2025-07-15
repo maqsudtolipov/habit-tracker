91c493bdec1c7811b9dd779448c0c63253718dd9
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-15 15:55:44 +0500

    Style: Add confirm on click for HabitCard

--- src/features/habits/components/HabitCard.tsx
+++ src/features/habits/components/HabitCard.tsx
@@ -2,6 +2,9 @@ import type {Habit} from "@/features/habits/types.ts";
 import {Checkbox} from "@/components/ui/checkbox.tsx";
 import DatePicker from "@/shared/ui/DatePicker.tsx";
 import {useState} from "react";
+import {Label} from "@/components/ui/label.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {CalendarIcon} from "lucide-react";
 
 interface HabitCardProps {
   habit: Habit;
@@ -15,32 +18,43 @@ const HabitCard = ({ habit }: HabitCardProps) => {
   };
 
   return (
-    <li
-      className={
-        "p-4 flex gap-3 rounded-lg outline-1 outline-gray-200 border border-l-4 border-gray-200/0" +
-        (isCompleted ? " border-gray-200/100" : "")
-      }
-    >
-      <div className="pt-1">
-        <Checkbox className="size-6" onCheckedChange={handleToggleCompleted} />
-      </div>
-      <div className="flex flex-col gap-4">
-        <div>
-          <h2 className="text-lg font-medium">{habit.name}</h2>
-          {/*{isCompleted ? (*/}
-          {/*  <p className="text-xs text-green-600">Completed</p>*/}
-          {/*) : (*/}
-          {/*  <p className="text-xs">Not Completed</p>*/}
-          {/*)}*/}
+    <li>
+      <Label
+        className={
+          "p-4 flex items-start gap-3 rounded-lg outline-1 outline-gray-200 border border-l-4 border-gray-200/0" +
+          (isCompleted ? " border-gray-200/100" : "")
+        }
+      >
+        <div className="pt-1">
+          <Checkbox
+            className="size-6"
+            onCheckedChange={handleToggleCompleted}
+          />
+        </div>
+        <div className="flex flex-col gap-4">
+          <div>
+            <h2 className="text-lg font-medium">{habit.name}</h2>
+            {/*{isCompleted ? (*/}
+            {/*  <p className="text-xs text-green-600">Completed</p>*/}
+            {/*) : (*/}
+            {/*  <p className="text-xs">Not Completed</p>*/}
+            {/*)}*/}
 
-          {habit.description && (
-            <p className="text-sm text-muted-foreground">{habit.description}</p>
-          )}
+            {habit.description && (
+              <p className="text-sm text-muted-foreground">
+                {habit.description}
+              </p>
+            )}
+          </div>
+          <div className="flex items-center gap-2">
+            <DatePicker />
+            <Button size="sm">
+              <CalendarIcon />
+              Confirm
+            </Button>
+          </div>
         </div>
-        <div>
-          <DatePicker />
-        </div>
-      </div>
+      </Label>
     </li>
   );
 };
