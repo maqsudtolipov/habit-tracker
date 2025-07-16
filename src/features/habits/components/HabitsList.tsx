e4ac525d9a1f854bf938d36e5147d3ebe379f17a
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-16 18:36:29 +0500

    Feature: Sort habits by newest ones

--- src/features/habits/components/HabitsList.tsx
+++ src/features/habits/components/HabitsList.tsx
@@ -1,13 +1,17 @@
 import {useSelector} from "react-redux";
 import type {RootState} from "@/app/store.ts";
 import HabitCard from "@/features/habits/components/HabitCard.tsx";
+import {compareAsc} from "date-fns";
 
 const HabitsList = () => {
   const habits = useSelector((state: RootState) => state.habits.habits);
+  const sortedHabitsByDate = [...habits].sort((a, b) =>
+    compareAsc(b.createdAt, a.createdAt),
+  );
 
   return (
     <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6">
-      {habits.map((habit) => (
+      {sortedHabitsByDate.map((habit) => (
         <HabitCard key={habit.id} habit={habit} />
       ))}
     </ul>
