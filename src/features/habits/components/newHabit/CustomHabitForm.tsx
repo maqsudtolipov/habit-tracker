53f3b23f7ec0ce634749427f561fdae66ee90a09
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-16 19:08:17 +0500

    Refactor: Remove unused states

--- src/features/habits/components/newHabit/CustomHabitForm.tsx
+++ src/features/habits/components/newHabit/CustomHabitForm.tsx
@@ -1,33 +1,7 @@
 import {Label} from "@/components/ui/label.tsx";
 import {Input} from "@/components/ui/input.tsx";
-import {useDispatch} from "react-redux";
-import {createNewHabit} from "@/features/habits/slice.ts";
-import {useState} from "react";
 
-const CustomHabitForm = ({
-  selectedTab,
-}: {
-  selectedTab: "custom" | "predefined";
-}) => {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const dispatch = useDispatch();
-
-  const handleSubmit = () => {
-    if (name.trim().length < 3) {
-      console.log("Invalid input");
-      return;
-    }
-
-    dispatch(
-      createNewHabit({
-        name: `${name} ${Math.floor(Math.random() * 100)}`,
-        description: description,
-        type: selectedTab,
-      }),
-    );
-  };
-
+const CustomHabitForm = () => {
   return (
     <>
       <p className="text-sm text-muted-foreground">
@@ -36,24 +10,13 @@ const CustomHabitForm = ({
       <div className="grid gap-4">
         <div className="grid gap-3">
           <Label htmlFor="name">* Name</Label>
-          <Input
-            id="name"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+          <Input id="name" name="name" />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="description">Description</Label>
-          <Input
-            id="description"
-            name="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
+          <Input id="description" name="description" />
         </div>
       </div>
-      <button onClick={handleSubmit}>Create new test habit</button>
     </>
   );
 };
