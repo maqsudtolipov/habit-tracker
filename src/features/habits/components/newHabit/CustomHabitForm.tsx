1fdee69f8acd0a9a2fcc15488d43bd8fa53dc1d8
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-16 18:30:34 +0500

    Feature: Add basic add functionality for new habits

--- src/features/habits/components/newHabit/CustomHabitForm.tsx
+++ src/features/habits/components/newHabit/CustomHabitForm.tsx
@@ -2,15 +2,22 @@ import {Label} from "@/components/ui/label.tsx";
 import {Input} from "@/components/ui/input.tsx";
 import {useDispatch} from "react-redux";
 import {createNewHabit} from "@/features/habits/slice.ts";
+import {useState} from "react";
 
 const CustomHabitForm = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
   const dispatch = useDispatch();
 
   const handleSubmit = () => {
+    if (name.trim().length < 3 || description.trim().length < 6) {
+      console.log("Invalid input");
+    }
+
     dispatch(
       createNewHabit({
-        name: "newHabit " + Math.floor(Math.random() * 100),
-        description: "",
+        name: `${name} ${Math.floor(Math.random() * 100)}`,
+        description: description,
         type: "custom",
       }),
     );
@@ -24,11 +31,21 @@ const CustomHabitForm = () => {
       <div className="grid gap-4">
         <div className="grid gap-3">
           <Label htmlFor="name">* Name</Label>
-          <Input id="name" name="name" />
+          <Input
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="description">Description</Label>
-          <Input id="description" name="description" />
+          <Input
+            id="description"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
         </div>
       </div>
       <button onClick={handleSubmit}>Create new test habit</button>
