239f7174dddd7474f67f258ba7bac71553071ae7
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-16 16:24:46 +0500

    Style: Update header layout for better user flow

--- src/features/habits/components/Header.tsx
+++ src/features/habits/components/Header.tsx
@@ -1,17 +1,25 @@
 import DailySummary from "@/features/habits/components/DailySummary.tsx";
 import DatePicker from "@/shared/ui/DatePicker.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {Plus} from "lucide-react";
 
 const Header = () => {
   return (
-    <div className="p-2 pl-4 mb-6 bg-gray-50 outline-2 outline-gray-200 rounded-lg">
-      <p>
-        <span className="inline-flex items-center gap-2">
-          🔥 For
-          <DatePicker />
-        </span>
-        <DailySummary />
-      </p>
-    </div>
+    <>
+      <div className="p-2 pl-4 mb-6 bg-gray-50 outline-2 outline-gray-200 rounded-lg">
+        <p>
+          <span>🔥 For [DATE] </span>
+          <DailySummary />
+        </p>
+      </div>
+
+      <div className="flex justify-end">
+        <DatePicker />
+        <Button size="sm" className="mb-4">
+          <Plus /> New Habit
+        </Button>
+      </div>
+    </>
   );
 };
 
