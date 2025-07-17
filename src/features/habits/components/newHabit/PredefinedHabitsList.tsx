9de41341718a5aa101a8f53d681f91a97932ec63
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-17 11:37:12 +0500

    Feature: Render 20 predefined list of habits

--- src/features/habits/components/newHabit/PredefinedHabitsList.tsx
+++ src/features/habits/components/newHabit/PredefinedHabitsList.tsx
@@ -1,4 +1,5 @@
 import {PREDEFINED_HABITS} from "@/features/habits/constants.ts";
+import {ScrollArea} from "@/components/ui/scroll-area.tsx";
 
 const PredefinedHabitsList = () => {
   return (
@@ -6,14 +7,16 @@ const PredefinedHabitsList = () => {
       <p className="text-sm text-muted-foreground">
         Choose one of the predefined habits here.
       </p>
-      <ul>
-        {PREDEFINED_HABITS.map((habit) => (
-          <li key={habit.id}>
-            <p>{habit.name}</p>
-            <p>{habit.description}</p>
-          </li>
-        ))}
-      </ul>
+      <ScrollArea className='h-40 rounded-md border'>
+        <ul className='flex flex-col gap-1 p-4'>
+          {PREDEFINED_HABITS.map((habit) => (
+            <li key={habit.id} className='border-b pb-1'>
+              <p className='text-black font-medium'>{habit.name}</p>
+              <p className='text-sm'>{habit.description}</p>
+            </li>
+          ))}
+        </ul>
+      </ScrollArea>
     </>
   );
 };
