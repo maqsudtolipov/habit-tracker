a3e444eae941e977d092427d6bb7b1e72b7e26cf
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-17 16:09:30 +0500

    Feature: Edit Habits

--- src/features/habits/components/habitCard/HabitControls.tsx
+++ src/features/habits/components/habitCard/HabitControls.tsx
@@ -2,10 +2,11 @@ import {Button} from "@/components/ui/button.tsx";
 import {Pencil, Trash2} from "lucide-react";
 import ConfirmDialog from "@/shared/ui/ConfirmDialog.tsx";
 import {useDispatch} from "react-redux";
-import {deleteHabit} from "@/features/habits/slice.ts";
+import {deleteHabit, editHabit} from "@/features/habits/slice.ts";
 import EditHabitDialog from "@/shared/ui/EditHabitDialog.tsx";
 import type {FormEvent} from "react";
 import type {Habit} from "@/features/habits/types.ts";
+import {MAX_DESCRIPTION_LENGTH, MAX_NAME_LENGTH,} from "@/features/habits/constants.ts";
 
 interface HabitControlsProps {
   habit: Habit;
@@ -14,10 +15,33 @@ interface HabitControlsProps {
 const HabitControls = ({ habit }: HabitControlsProps) => {
   const dispatch = useDispatch();
 
+  // INFO: Edit and Create forms can be merged
   const handleEditHabit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log("Submit edit");
+    const formData = new FormData(e.currentTarget);
+    const data = {
+      name: formData.get("name")?.toString().trim() ?? "",
+      description: formData.get("description")?.toString().trim() ?? "",
+    };
+
+    // Validate
+    if (
+      !data.name ||
+      data.name.length < 3 ||
+      data.name.length > MAX_NAME_LENGTH ||
+      data.description.length > MAX_DESCRIPTION_LENGTH
+    ) {
+      return;
+    }
+
+    dispatch(
+      editHabit({
+        id: habit.id,
+        name: `${data.name} ${Math.floor(Math.random() * 100)}`,
+        description: data.description,
+      }),
+    );
   };
 
   const handleDeleteHabit = () => {
