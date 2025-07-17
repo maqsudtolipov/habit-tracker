4112f12c0495268fec9bf8b6353c1d6abc72c63e
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-17 14:22:18 +0500

    Refactor: Split new habit forms into two

--- src/features/habits/components/newHabit/CustomHabitForm.tsx
+++ src/features/habits/components/newHabit/CustomHabitForm.tsx
@@ -1,18 +1,66 @@
 import {Label} from "@/components/ui/label.tsx";
 import {Input} from "@/components/ui/input.tsx";
+import {DialogClose, DialogFooter} from "@/components/ui/dialog.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {useDispatch} from "react-redux";
+import type {FormEvent} from "react";
+import {MAX_DESCRIPTION_LENGTH, MAX_NAME_LENGTH,} from "@/features/habits/constants.ts";
+import {createNewHabit} from "@/features/habits/slice.ts";
 
-const CustomHabitForm = () => {
+const CustomHabitForm = ({ onCloseDialog }) => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const data = {
+      name: formData.get("name")?.toString().trim() ?? "",
+      description: formData.get("description")?.toString().trim() ?? "",
+    };
+
+    // Validate
+    if (
+      !data.name ||
+      data.name.length < 3 ||
+      data.name.length > MAX_NAME_LENGTH ||
+      data.description.length > MAX_DESCRIPTION_LENGTH
+    ) {
+      return;
+    }
+
+    dispatch(
+      createNewHabit({
+        name: `${data.name} ${Math.floor(Math.random() * 100)}`,
+        description: data.description,
+        type: "custom",
+      }),
+    );
+
+    onCloseDialog();
+  };
+
   return (
-    <div className="h-60 flex flex-col gap-4">
-      <div className="grid gap-3">
-        <Label htmlFor="name">* Name</Label>
-        <Input id="name" name="name" />
+    <form onSubmit={handleSubmit}>
+      <div className="h-60 flex flex-col gap-4">
+        <div className="grid gap-3">
+          <Label htmlFor="name">* Name</Label>
+          <Input id="name" name="name" />
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="description">Description</Label>
+          <Input id="description" name="description" />
+        </div>
       </div>
-      <div className="grid gap-3">
-        <Label htmlFor="description">Description</Label>
-        <Input id="description" name="description" />
-      </div>
-    </div>
+
+      {/* NOTE: DUPLICATED on each tab */}
+      <DialogFooter>
+        <DialogClose asChild>
+          <Button variant="outline">Cancel</Button>
+        </DialogClose>
+        <Button type="submit">Save changes</Button>
+      </DialogFooter>
+    </form>
   );
 };
 
--- src/features/habits/components/newHabit/NewHabitTabs.tsx
+++ src/features/habits/components/newHabit/NewHabitTabs.tsx
@@ -1,100 +1,33 @@
 import {Tabs, TabsContent, TabsList, TabsTrigger,} from "@/components/ui/tabs.tsx";
 import CustomHabitForm from "@/features/habits/components/newHabit/CustomHabitForm.tsx";
-import {type FormEvent, useState} from "react";
+import {useState} from "react";
 import PredefinedHabitsList from "@/features/habits/components/newHabit/PredefinedHabitsList.tsx";
-import {useDispatch} from "react-redux";
-import {createNewHabit} from "@/features/habits/slice.ts";
-import {DialogClose, DialogFooter} from "@/components/ui/dialog.tsx";
-import {Button} from "@/components/ui/button.tsx";
-import {MAX_DESCRIPTION_LENGTH, MAX_NAME_LENGTH, PREDEFINED_HABITS,} from "@/features/habits/constants.ts";
 
 const NewHabitTabs = ({ onCloseDialog }) => {
   const [selectedTab, setSelectedTab] = useState("predefined");
   const [selectedHabitId, setSelectedHabitId] = useState<null | string>(null);
-  const dispatch = useDispatch();
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (selectedTab === "custom") {
-      const formData = new FormData(e.currentTarget);
-      const data = {
-        name: formData.get("name")?.toString().trim() ?? "",
-        description: formData.get("description")?.toString().trim() ?? "",
-      };
-
-      // Validate
-      if (
-        !data.name ||
-        data.name.length < 3 ||
-        data.name.length > MAX_NAME_LENGTH ||
-        data.description.length > MAX_DESCRIPTION_LENGTH
-      ) {
-        return;
-      }
-
-      dispatch(
-        createNewHabit({
-          name: `${data.name} ${Math.floor(Math.random() * 100)}`,
-          description: data.description,
-          type: "custom",
-        }),
-      );
-
-      onCloseDialog();
-    }
-
-    if (selectedTab === "predefined") {
-      const selectedHabit = PREDEFINED_HABITS.find(
-        (habit) => habit.id === selectedHabitId,
-      );
-
-      if (!selectedHabit) return;
-
-      dispatch(
-        createNewHabit({
-          name: `${selectedHabit.name} ${Math.floor(Math.random() * 100)}`,
-          description: selectedHabit.description,
-          type: "predefined",
-        }),
-      );
-
-      onCloseDialog();
-    }
-  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Tabs
-        className="w-full gap-4"
-        value={selectedTab}
-        onValueChange={setSelectedTab}
-      >
-        <TabsList>
-          <TabsTrigger value="predefined">Predefined</TabsTrigger>
-          <TabsTrigger value="custom">Custom</TabsTrigger>
-        </TabsList>
-        <TabsContent className="flex flex-col gap-4" value="custom">
-          <CustomHabitForm />
-        </TabsContent>
-        <TabsContent
-          className="text-sm text-muted-foreground"
-          value="predefined"
-        >
-          <PredefinedHabitsList
-            selectedHabitId={selectedHabitId}
-            onSelectHabit={setSelectedHabitId}
-          />
-        </TabsContent>
-
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DialogClose>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </Tabs>
-    </form>
+    <Tabs
+      className="w-full gap-4"
+      value={selectedTab}
+      onValueChange={setSelectedTab}
+    >
+      <TabsList>
+        <TabsTrigger value="predefined">Predefined</TabsTrigger>
+        <TabsTrigger value="custom">Custom</TabsTrigger>
+      </TabsList>
+      <TabsContent className="flex flex-col gap-4" value="custom">
+        <CustomHabitForm onCloseDialog={onCloseDialog} />
+      </TabsContent>
+      <TabsContent className="text-sm text-muted-foreground" value="predefined">
+        <PredefinedHabitsList
+          selectedHabitId={selectedHabitId}
+          onSelectHabit={setSelectedHabitId}
+          onCloseDialog={onCloseDialog}
+        />
+      </TabsContent>
+    </Tabs>
   );
 };
 
