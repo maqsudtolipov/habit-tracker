04d0707f1a801ac48f6fa16478dc7cb6c64a8ea2
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-17 12:58:09 +0500

    Feature: Lover form submittion states

--- src/features/habits/components/newHabit/NewHabitDialog.tsx
+++ src/features/habits/components/newHabit/NewHabitDialog.tsx
@@ -1,46 +1,9 @@
-import {Button} from "@/components/ui/button.tsx";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog.tsx";
+import {Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger,} from "@/components/ui/dialog.tsx";
 
-import {type FormEvent, type ReactNode, useState} from "react";
+import {type ReactNode} from "react";
 import NewHabitTabs from "@/features/habits/components/newHabit/NewHabitTabs.tsx";
-import {createNewHabit} from "@/features/habits/slice.ts";
-import {useDispatch} from "react-redux";
 
 const NewHabitDialog = ({ children }: { children: ReactNode }) => {
-  const [selectedHabitId, setSelectedHabitId] = useState<null | string>(null);
-  const dispatch = useDispatch();
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.currentTarget);
-    const data = {
-      name: formData.get("name"),
-      description: formData.get("description"),
-    };
-
-    // Validate
-    if (!data.name || data.name.trim().length < 3) {
-      return;
-    }
-
-    dispatch(
-      createNewHabit({
-        name: `${data.name} ${Math.floor(Math.random() * 100)}`,
-        description: data.description,
-        type: "custom",
-      }),
-    );
-  };
-
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -49,19 +12,9 @@ const NewHabitDialog = ({ children }: { children: ReactNode }) => {
           <DialogTitle>Create a new habit</DialogTitle>
         </DialogHeader>
 
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <NewHabitTabs
-            selectedHabitId={selectedHabitId}
-            onSelectHabit={setSelectedHabitId}
-          />
-
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </form>
+        <div className="flex flex-col gap-4">
+          <NewHabitTabs />
+        </div>
       </DialogContent>
     </Dialog>
   );
--- src/features/habits/components/newHabit/NewHabitTabs.tsx
+++ src/features/habits/components/newHabit/NewHabitTabs.tsx
@@ -1,38 +1,75 @@
 import {Tabs, TabsContent, TabsList, TabsTrigger,} from "@/components/ui/tabs.tsx";
 import CustomHabitForm from "@/features/habits/components/newHabit/CustomHabitForm.tsx";
-import {useState} from "react";
+import {type FormEvent, useState} from "react";
 import PredefinedHabitsList from "@/features/habits/components/newHabit/PredefinedHabitsList.tsx";
+import {useDispatch} from "react-redux";
+import {createNewHabit} from "@/features/habits/slice.ts";
+import {DialogClose, DialogFooter} from "@/components/ui/dialog.tsx";
+import {Button} from "@/components/ui/button.tsx";
 
 // I dont like prop drilling here
-const NewHabitTabs = ({
-  selectedHabitId,
-  onSelectHabit,
-}: {
-  selectedHabitId: string;
-  onSelectHabit: () => void;
-}) => {
+const NewHabitTabs = () => {
   const [selectedTab, setSelectedTab] = useState("predefined");
+  const [selectedHabitId, setSelectedHabitId] = useState<null | string>(null);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log("Submit it hit");
+
+    const formData = new FormData(e.currentTarget);
+    const data = {
+      name: formData.get("name"),
+      description: formData.get("description"),
+    };
+
+    // Validate
+    if (!data.name || data.name.trim().length < 3) {
+      return;
+    }
+
+    dispatch(
+      createNewHabit({
+        name: `${data.name} ${Math.floor(Math.random() * 100)}`,
+        description: data.description,
+        type: "custom",
+      }),
+    );
+  };
 
   return (
-    <Tabs
-      className="w-full gap-4"
-      value={selectedTab}
-      onValueChange={setSelectedTab}
-    >
-      <TabsList>
-        <TabsTrigger value="predefined">Predefined</TabsTrigger>
-        <TabsTrigger value="custom">Custom</TabsTrigger>
-      </TabsList>
-      <TabsContent className="flex flex-col gap-4" value="custom">
-        <CustomHabitForm selectedTab={selectedTab} />
-      </TabsContent>
-      <TabsContent className="text-sm text-muted-foreground" value="predefined">
-        <PredefinedHabitsList
-          selectedHabitId={selectedHabitId}
-          onSelectHabit={onSelectHabit}
-        />
-      </TabsContent>
-    </Tabs>
+    <form onSubmit={handleSubmit}>
+      <Tabs
+        className="w-full gap-4"
+        value={selectedTab}
+        onValueChange={setSelectedTab}
+      >
+        <TabsList>
+          <TabsTrigger value="predefined">Predefined</TabsTrigger>
+          <TabsTrigger value="custom">Custom</TabsTrigger>
+        </TabsList>
+        <TabsContent className="flex flex-col gap-4" value="custom">
+          <CustomHabitForm selectedTab={selectedTab} />
+        </TabsContent>
+        <TabsContent
+          className="text-sm text-muted-foreground"
+          value="predefined"
+        >
+          <PredefinedHabitsList
+            selectedHabitId={selectedHabitId}
+            onSelectHabit={setSelectedHabitId}
+          />
+        </TabsContent>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </Tabs>
+    </form>
   );
 };
 
