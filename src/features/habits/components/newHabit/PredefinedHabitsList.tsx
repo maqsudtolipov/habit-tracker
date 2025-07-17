2ab3f6cb3eed3e6641229caa088c2e346714b332
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-17 12:37:02 +0500

    Feature: Make predefined habits selectable

--- src/features/habits/components/newHabit/PredefinedHabitsList.tsx
+++ src/features/habits/components/newHabit/PredefinedHabitsList.tsx
@@ -1,16 +1,27 @@
 import {PREDEFINED_HABITS} from "@/features/habits/constants.ts";
 import {ScrollArea} from "@/components/ui/scroll-area.tsx";
+import {useState} from "react";
 
 const PredefinedHabitsList = () => {
+  const [selectedHabitId, setSelectedHabitId] = useState<null | string>(null);
+
   return (
     <ScrollArea className="h-60 rounded-md border">
-      <ul className="flex flex-col gap-1 p-4">
-        {PREDEFINED_HABITS.map((habit) => (
-          <li key={habit.id} className="border-b pb-1">
-            <p className="text-black font-medium">{habit.name}</p>
-            <p className="text-sm">{habit.description}</p>
-          </li>
-        ))}
+      <ul className="flex flex-col gap-2 p-2 pr-4">
+        {PREDEFINED_HABITS.map((habit) => {
+          const isSelected = habit.id === selectedHabitId;
+
+          return (
+            <li
+              key={habit.id}
+              className={`p-2 rounded select-none ${isSelected && "outline-2 outline-black/50"}`}
+              onClick={() => setSelectedHabitId(habit.id)}
+            >
+              <p className="text-black font-medium">{habit.name}</p>
+              <p className="text-sm">{habit.description}</p>
+            </li>
+          );
+        })}
       </ul>
     </ScrollArea>
   );
