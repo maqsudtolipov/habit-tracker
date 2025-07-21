277e18d116e4591e3304ecacb0f5823aa6464dec
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-21 11:51:43 +0500

    Package: Install shadcn/ui sooner

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import AppRoutes from "./app/AppRoutes.tsx";
 import {Provider} from "react-redux";
 import {store} from "@/app/store.ts";
+import {Toaster} from "@/components/ui/sonner.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <AppRoutes />
+      <Toaster />
     </Provider>
   </StrictMode>,
 );
