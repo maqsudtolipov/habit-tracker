d26e23a9c792e3412c0a37a67ca9e0a22431d6d7
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-22 10:34:05 +0500

    Feature: Change 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -3,9 +3,8 @@ import {Link} from "react-router-dom";
 const NotFoundPage = () => {
   return (
     <div className="px-10 py-20 flex flex-col items-center justify-center">
-      <h2 className="text-2xl font-medium">
-        404: This Habit Went on Vacation 🌴
-      </h2>
+      <span className="pb-8 text-9xl"> 🌴</span>
+      <h2 className="text-2xl font-medium">404: This Habit Went on Vacation</h2>
       <Link className="hover:underline" to="/">
         {" "}
         Take me back to safety
