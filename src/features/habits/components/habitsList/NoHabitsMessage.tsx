e032d7fe2c01914f069d5d5a860b9a10456c8774
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-22 16:43:50 +0500

    Style: Add border for no habit message

--- src/features/habits/components/habitsList/NoHabitsMessage.tsx
+++ src/features/habits/components/habitsList/NoHabitsMessage.tsx
@@ -2,7 +2,7 @@ import NewHabitDialog from "@/features/habits/components/habitCard/newHabit/NewH
 
 const NoHabitsMessage = () => {
   return (
-    <div className="py-12 flex flex-col items-center justify-center gap-4 text-center rounded-lg outline-1 outline-gray-200">
+    <div className="px-4 py-12 flex flex-col items-center justify-center gap-4 text-center rounded-lg outline-1 outline-gray-200">
       <h2 className="text-2xl font-semibold text-neutral-800">No Habits Yet</h2>
       <p className="text-neutral-600">
         Start by creating your first habit to track your progress.
