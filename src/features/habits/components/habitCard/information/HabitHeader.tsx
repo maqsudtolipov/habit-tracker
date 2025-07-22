3c9fbdd097363c14f86321d04ff9d1b67856dec9
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-22 17:06:30 +0500

    Refactor: Split Header information components

---
+++ src/features/habits/components/habitCard/information/HabitHeader.tsx
@@ -0,0 +1,65 @@
+import {useDispatch, useSelector} from "react-redux";
+import type {RootState} from "@/app/store.ts";
+import type {Habit} from "@/features/habits/types.ts";
+import {toggleProgressStatus} from "@/features/progress/slice.ts";
+import getFormatedDate from "@/shared/utils/getFormatedDate.ts";
+import {Checkbox} from "@/shared/ui/checkbox.tsx";
+import {Box, Sparkle} from "lucide-react";
+import HabitControlButtons from "@/features/habits/components/habitCard/information/HabitControlButtons.tsx";
+
+interface HabitHeaderProps {
+  habit: Habit;
+  isCompleted: boolean;
+}
+
+const HabitHeader = ({ habit, isCompleted }: HabitHeaderProps) => {
+  const selectedDate = useSelector(
+    (state: RootState) => state.habits.selectedDate,
+  );
+  const dispatch = useDispatch();
+
+  const handleToggleCompleted = () => {
+    dispatch(
+      toggleProgressStatus({
+        date: getFormatedDate(selectedDate),
+        habitId: habit.id,
+      }),
+    );
+  };
+
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-start gap-3">
+        <div className="pt-1">
+          <Checkbox
+            id={`checkbox-${habit.id}`}
+            className="size-6"
+            checked={isCompleted}
+            onCheckedChange={handleToggleCompleted}
+          />
+        </div>
+        <div>
+          <h2
+            className="text-lg font-medium -mb-1 text-neutral-900"
+            id={`habit-${habit.id}-name`}
+          >
+            {habit.name}
+          </h2>
+          <p className="flex items-center gap-1 text-xs font-normal text-neutral-600">
+            {habit.type === "predefined" ? (
+              <Box className="size-3" />
+            ) : (
+              <Sparkle className="size-3" />
+            )}
+
+            {habit.type}
+          </p>
+        </div>
+      </div>
+
+      {habit.type === "custom" && <HabitControlButtons habit={habit} />}
+    </div>
+  );
+};
+
+export default HabitHeader;
--- src/features/habits/components/habitCard/information/HabitInformation.tsx
+++ src/features/habits/components/habitCard/information/HabitInformation.tsx
@@ -1,11 +1,6 @@
-import {Checkbox} from "@/shared/ui/checkbox.tsx";
-import {toggleProgressStatus} from "@/features/progress/slice.ts";
-import {useDispatch, useSelector} from "react-redux";
 import type {Habit} from "@/features/habits/types.ts";
-import HabitControlButtons from "@/features/habits/components/habitCard/information/HabitControlButtons.tsx";
-import type {RootState} from "@/app/store.ts";
-import getFormatedDate from "@/shared/utils/getFormatedDate.ts";
-import {Box, Sparkle} from "lucide-react";
+import HabitHeader from "@/features/habits/components/habitCard/information/HabitHeader.tsx";
+import HabitDescription from "@/features/habits/components/habitCard/information/HabitDescription.tsx";
 
 interface HabitInformationProps {
   habit: Habit;
@@ -13,56 +8,11 @@ interface HabitInformationProps {
 }
 
 const HabitInformation = ({ habit, isCompleted }: HabitInformationProps) => {
-  const selectedDate = useSelector(
-    (state: RootState) => state.habits.selectedDate,
-  );
-  const dispatch = useDispatch();
-
-  const handleToggleCompleted = () => {
-    dispatch(
-      toggleProgressStatus({
-        date: getFormatedDate(selectedDate),
-        habitId: habit.id,
-      }),
-    );
-  };
-
   return (
     <div className="mb-2 flex flex-col gap-2 flex-1">
-      <div className="flex items-center justify-between">
-        <div className="flex items-start gap-3">
-          <div className="pt-1">
-            <Checkbox
-              id={`checkbox-${habit.id}`}
-              className="size-6"
-              checked={isCompleted}
-              onCheckedChange={handleToggleCompleted}
-            />
-          </div>
-          <div>
-            <h2
-              className="text-lg font-medium -mb-1 text-neutral-900"
-              id={`habit-${habit.id}-name`}
-            >
-              {habit.name}
-            </h2>
-            <p className="flex items-center gap-1 text-xs font-normal text-neutral-600">
-              {habit.type === "predefined" ? (
-                <Box className="size-3" />
-              ) : (
-                <Sparkle className="size-3" />
-              )}
-
-              {habit.type}
-            </p>
-          </div>
-        </div>
-
-        {habit.type === "custom" && <HabitControlButtons habit={habit} />}
-      </div>
-
+      <HabitHeader habit={habit} isCompleted={isCompleted} />
       {habit.description && (
-        <p className="text-sm text-neutral-500">{habit.description}</p>
+        <HabitDescription description={habit.description} />
       )}
     </div>
   );
