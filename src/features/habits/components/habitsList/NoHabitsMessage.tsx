5832ff0aefdaa4b49f871f9e85467d7a6d842e09
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-22 16:37:33 +0500

    Style: Minor changes for no habit message

--- src/features/habits/components/habitsList/NoHabitsMessage.tsx
+++ src/features/habits/components/habitsList/NoHabitsMessage.tsx
@@ -2,7 +2,7 @@ import NewHabitDialog from "@/features/habits/components/habitCard/newHabit/NewH
 
 const NoHabitsMessage = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 py-8">
+    <div className=" py-8 flex flex-col items-center justify-center gap-4 text-center">
       <h2 className="text-2xl font-semibold text-neutral-800">No Habits Yet</h2>
       <p className="text-neutral-600">
         Start by creating your first habit to track your progress.
