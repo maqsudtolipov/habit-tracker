1be7e4202d9f4855224b3c02718a284b27bdbe0c
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-31 17:59:07 +0500

    Feature: Setup redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,24 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import habitsSlice from "@/features/habits/slice.ts";
 import progressSlice from "@/features/progress/slice.ts";
+import storage from "redux-persist/lib/storage";
+import {persistReducer} from "redux-persist";
+
+const rootReducer = combineReducers({
+  habits: habitsSlice.reducer,
+  progress: progressSlice.reducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["habits", "progress"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    habits: habitsSlice.reducer,
-    progress: progressSlice.reducer,
-  },
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
