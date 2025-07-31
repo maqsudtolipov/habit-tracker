9df143c492308c9d9204710b4f7e0be419e30e6a
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-31 21:12:45 +0500

    Fix: Remove unused div

--- src/features/habits/components/Summary/SummaryActions.tsx
+++ src/features/habits/components/Summary/SummaryActions.tsx
@@ -13,13 +13,12 @@ const SummaryActions = () => {
 
   return (
     <div className="flex items-center justify-end flex-1">
-      <div>
-        <DatePicker
-          value={new Date(selectedDate)}
-          onChange={handleDateChange}
-          aria-label="Pick date to see habit progress"
-        />
-      </div>
+      <DatePicker
+        value={new Date(selectedDate)}
+        onChange={handleDateChange}
+        aria-label="Pick date to see habit progress"
+      />
+
       <NewHabitDialog />
     </div>
   );
