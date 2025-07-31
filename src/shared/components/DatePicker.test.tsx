94b890256ad03af828671d36de9f94a4afcdb8e1
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-07-31 15:49:57 +0500

    Test: DatePicker.tsx should call onChange() when a new date is selected

--- src/shared/components/DatePicker.test.tsx
+++ src/shared/components/DatePicker.test.tsx
@@ -1,6 +1,7 @@
 import {describe, expect, it} from "vitest";
 import {render, screen} from "@testing-library/react";
 import DatePicker from "@/shared/components/DatePicker.tsx";
+import userEvent from "@testing-library/user-event";
 
 describe("<DatePicker />", () => {
   it("should render trigger button", () => {
@@ -14,7 +15,35 @@ describe("<DatePicker />", () => {
     expect(el).toBeInTheDocument();
   });
 
-  it("should open and close when trigger button is clicked", () => {});
+  it("should open and close when trigger button is clicked", async () => {
+    const user = userEvent.setup();
+    render(<DatePicker value={new Date()} onChange={() => {}} />);
 
-  it("should call onChange() fn when date is picked", () => {});
+    const button = screen.getByRole("button", { name: /pick a date/i });
+
+    // Open dialog
+    await user.click(button);
+    expect(screen.getByRole("dialog")).toBeInTheDocument();
+
+    // Close dialog
+    await user.click(button);
+
+    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+  });
+
+  it("should call onChange() when a new date is selected", async () => {
+    const user = userEvent.setup();
+    const handleChange = vi.fn();
+
+    const initialDate = new Date("2025-07-01");
+    render(<DatePicker value={initialDate} onChange={handleChange} />);
+
+    const button = screen.getByRole("button", { name: /pick a date/i });
+    await user.click(button);
+
+    const newDateBtn = screen.getByLabelText("Tuesday, July 15th, 2025");
+    await user.click(newDateBtn);
+
+    expect(handleChange).toHaveBeenCalledTimes(1);
+  });
 });
