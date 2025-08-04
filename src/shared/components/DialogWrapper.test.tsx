73d07defcb6f6e2715b9a0a072fff09c1c6fa725
Maqsud Tolipov <95850553+maqsudtolipov@users.noreply.github.com>
2025-08-04 15:53:55 +0500

    Test: DialogWrapper should render trigger and title

---
+++ src/shared/components/DialogWrapper.test.tsx
@@ -0,0 +1,35 @@
+import {expect, it} from "vitest";
+import {render, screen} from "@testing-library/react";
+import DialogWrapper from "@/shared/components/DialogWrapper.tsx";
+import userEvent from "@testing-library/user-event";
+
+describe("<DialogWrapper />", () => {
+  it("should render trigger and title", async () => {
+    const user = userEvent.setup();
+    render(
+      <DialogWrapper title="Edit Habit" trigger={<button>Edit dialog</button>}>
+        {(onClose) => (
+          <div>
+            <button onClick={onClose}>Update Habit</button>
+          </div>
+        )}
+      </DialogWrapper>,
+    );
+
+    // Renders trigger
+    const trigger = screen.getByRole("button", { name: /edit dialog/i });
+    expect(trigger).toBeInTheDocument();
+
+    await user.click(trigger);
+
+    expect(
+      screen.getByRole("button", { name: /Update Habit/i }),
+    ).toBeInTheDocument();
+  });
+
+  it("should open on trigger clicked", () => {});
+
+  it("should pass onClose correctly to the children", () => {});
+
+  it("should ", () => {});
+});
